390d2d320ec39413a28409388622b61fa6ff6350
Nikhil Sharma <ncksharma8@gmail.com>
2019-02-24 08:48:20 +0530

    Add files via upload

--- ReportingTool.py
+++ ReportingTool.py
@@ -28,7 +28,9 @@ result2 = execute_q("""select authors.name,authorViewSums.authorView
                           where authors.id=authorViewSums.author""")
 
 # Days on which more than 1% of requests lead to errors.
-result3 = execute_q("""select d as Day,m as Month,y as Year from error_Matrix
+result3 = execute_q("""select d as Day,m as Month,y as Year,
+                       (err*1.0/total_Requests)*100 as Error
+                        from error_Matrix
                         where (err*1.0/total_Requests)*100>1;""")
 print("\nThe most popular three articles of all time.\n")
 res = pd.DataFrame(data=result1, columns=['Article', 'Views'])
@@ -39,7 +41,7 @@ res = pd.DataFrame(data=result2, columns=['Author', 'Views'])
 print(res)
 print("\n")
 print("Days on which more than 1 percent of requests lead to errors.\n")
-res = pd.DataFrame(data=result3, columns=['Day', 'Month', 'Year'])
+res = pd.DataFrame(data=result3, columns=['Day', 'Month', 'Year','ERROR%'])
 res = res.astype(int)
 print(res)
 print("\n")
